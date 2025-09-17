d39b67c106f98328c727e30aa0e8efe5f0d6c06b
tallulahh <tallulah.carlisle@gmail.com>
2025-09-17 17:37:32 +0100

    update: workflow

--- sign.sh
+++ sign.sh
@@ -6,4 +6,5 @@ git config --global user.email "tallulah.carlisle@gmail.com"
 git config --global commit.gpgsign true
 git config --global tag.gpgsign true
 git config --global user.signingkey $SIGNING_KEY
+git config --global push.gpgsign true
 echo nextReleaseVersion=$NEXT_RELEASE_VERSION >> build.env
