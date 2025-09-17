1177c56889fea47605dd8642aff74498fabe3a94
tallulahh <30673953+tallulahh@users.noreply.github.com>
2025-09-17 09:02:37 +0100

    build: test (#11)
    * feat: Test signed commits
    
    * fix: workflow
    
    * fix: workflow
    
    * fix: workflow
    
    * fix
    
    * fix
    
    * add
    
    * add gpg

--- sign.sh
+++ sign.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+NEXT_RELEASE_VERSION=$1
+SIGNING_KEY=$2
 git config --global user.name "tallulahh"
 git config --global user.email "tallulah.carlisle@gmail.com"
 git config --global commit.gpgsign true
 git config --global tag.gpgsign true
-git config --global user.signingkey ${{ secrets.GPG_SIGNING_KEY }}
-echo nextReleaseVersion=${nextRelease.version} >> build.env
+git config --global user.signingkey $SIGNING_KEY
+echo nextReleaseVersion=$NEXT_RELEASE_VERSION >> build.env
